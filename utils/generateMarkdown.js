fea60173f82c8225e37240c6c4fedb056e874c5a
NG <nicholeguan2@gmail.com>
2022-10-20 23:19:11 -0700

    add functionality to renderLicenseBadge() and add badges to generated README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,40 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+  if (license == "None") {
+    return ""
+  }
+  else if (license == "Apache 2.0 License") {
+    return "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)"
+  }
+  else if (license == "Boost Software License 1.0") {
+    return "![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)"
+  }
+  else if (license == "BSD 3-Clause License") {
+    return "![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)"
+  }
+  else if (license == "BSD 2-Clause License") {
+    return "![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)"
+  }
+  else if (license == "GNU General Public License v3.0") {
+    return "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)"
+  }
+  else if (license == "GNU General Public License v2.0") {
+    return "![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)"
+  }
+  else if (license == "GNU Affero General Public v3.0") {
+    return "![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)"
+  }
+  else if (license == "MIT License") {
+    return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)"
+  }
+  else if (license == "Mozilla Public License 2.0") {
+    return "![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)"
+  }
+  else if (license == "The Unlicense") {
+    return "![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)"
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -28,6 +62,7 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  \n${renderLicenseBadge(data.license)}
   \n## Description
   \n${data.description}
   \n## Table of Contents
