ee738f16ad9b648648f872918cc1e7d66916f954
NG <nicholeguan2@gmail.com>
2022-10-20 20:32:54 -0700

    change questions array to contain question objects and add inquirer prompt into init() to test writeToFile

--- index.js
+++ index.js
@@ -1,25 +1,72 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+//const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "What is your GitHub username?",
-    "What is your email address?",
-    "What is your project's name?",
-    "Please write a short description of your project.",
-    "What kind of license should your project have?",
-    "What does the user need to know about installing the repo?",
-    "What command should be run to run tests?",
-    "What does the user need to know about using the repo?",
-    "What does the user need to know about contributing to the repo?"
+    {
+        type: 'input',
+        name: 'username',
+        message: "What is your GitHub username?"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email address?"
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is your project's name?"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Please write a short description of your project."
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: "What kind of license should your project have?"
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: "What does the user need to know about installing the repo?"
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: "What command should be run to run tests?"
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "What does the user need to know about using the repo?"
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: "What does the user need to know about contributing to the repo?"
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('Successfully created a README file'))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            writeToFile("generated-README.md", "tessstttt")
+        //console.log(answers);
+        });
+}
 
 // Function call to initialize app
 init();
