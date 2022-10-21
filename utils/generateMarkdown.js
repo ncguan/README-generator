14f95ad58ce90fe59c6e1b752d33750fb9594258
NG <nicholeguan2@gmail.com>
2022-10-20 23:37:33 -0700

    add functionality to renderLicenseLink and add link to badges on generated README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,34 +5,34 @@ function renderLicenseBadge(license) {
     return ""
   }
   else if (license == "Apache 2.0 License") {
-    return "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)"
+    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]"
   }
   else if (license == "Boost Software License 1.0") {
-    return "![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)"
+    return "[![License](https://img.shields.io/badge/License-Boost_1.0-blue.svg)]"
   }
   else if (license == "BSD 3-Clause License") {
-    return "![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)"
+    return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]"
   }
   else if (license == "BSD 2-Clause License") {
-    return "![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)"
+    return "[![License](https://img.shields.io/badge/License-BSD_2--Clause-blue.svg)]"
   }
   else if (license == "GNU General Public License v3.0") {
-    return "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)"
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]"
   }
   else if (license == "GNU General Public License v2.0") {
-    return "![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)"
+    return "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)]"
   }
   else if (license == "GNU Affero General Public v3.0") {
-    return "![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)"
+    return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)]"
   }
   else if (license == "MIT License") {
-    return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)"
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)]"
   }
   else if (license == "Mozilla Public License 2.0") {
-    return "![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)"
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-blue.svg)]"
   }
   else if (license == "The Unlicense") {
-    return "![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)"
+    return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]"
   }
 }
 
@@ -44,6 +44,33 @@ function renderLicenseLink(license) {
   }
   else if (license == "Apache 2.0 License") {
     return "(https://opensource.org/licenses/Apache-2.0)"
+  }
+  else if (license == "Boost Software License 1.0") {
+    return "(https://www.boost.org/LICENSE_1_0.txt)"
+  }
+  else if (license == "BSD 3-Clause License") {
+    return "(https://opensource.org/licenses/BSD-3-Clause)"
+  }
+  else if (license == "BSD 2-Clause License") {
+    return "(https://opensource.org/licenses/BSD-2-Clause)"
+  }
+  else if (license == "GNU General Public License v3.0") {
+    return "(https://www.gnu.org/licenses/gpl-3.0)"
+  }
+  else if (license == "GNU General Public License v2.0") {
+    return "(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)"
+  }
+  else if (license == "GNU Affero General Public v3.0") {
+    return "(https://www.gnu.org/licenses/agpl-3.0)"
+  }
+  else if (license == "MIT License") {
+    return "(https://opensource.org/licenses/MIT)"
+  }
+  else if (license == "Mozilla Public License 2.0") {
+    return "(https://opensource.org/licenses/MPL-2.0)"
+  }
+  else if (license == "The Unlicense") {
+    return "(http://unlicense.org/)"
   }
 }
 
@@ -62,7 +89,7 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  \n${renderLicenseBadge(data.license)}
+  \n${renderLicenseBadge(data.license)}${renderLicenseLink(data.license)}
   \n## Description
   \n${data.description}
   \n## Table of Contents
