c6bdc6f086669a258d83c893bacf813846b71b4c
NG <nicholeguan2@gmail.com>
2022-10-20 21:14:45 -0700

    generate README with user input

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-//const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -55,7 +55,7 @@ const questions = [
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) =>
-    err ? console.log(err) : console.log('Successfully created a README file'))
+        err ? console.log(err) : console.log('Successfully created a README file'))
 }
 
 // TODO: Create a function to initialize app
@@ -63,8 +63,7 @@ function init() {
     inquirer
         .prompt(questions)
         .then((answers) => {
-            writeToFile("generated-README.md", "tessstttt")
-        //console.log(answers);
+            writeToFile("generated-README.md", generateMarkdown(answers))
         });
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,41 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  \n## Description
+  \n${data.description}
+  \n## Table of Contents
+  \n- [Installation](#installation)
+  \n- [Usage](#usage)
+  \n- [License](#license)
+  \n- [Contributing](#contributing)
+  \n- [Tests](#tests)
+  \n- [Questions](#questions)
+  \n## Installation
+  \nInstall the project by:
+  \n${data.install}
+  \n## Usage
+  \n${data.usage}
+  \n## License
+  \nThis project is licensed under the ${data.license}
+  \n## Contributing
+  \n${data.contributing}
+  \n## Tests
+  \nTo run tests, run the following command:
+  \n${data.test}
+  \n## Questions
+  \nIf you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at ${data.username}.
 `;
 }
 
