f5292464937af48a2d8ab33b433a9ff6ce753a2d
NG <nicholeguan2@gmail.com>
2022-10-20 23:05:47 -0700

    add functionality to renderLicenseSection()

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,11 +4,26 @@ function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  if (license == "None") {
+    return ""
+  }
+  else if (license == "Apache 2.0 License") {
+    return "(https://opensource.org/licenses/Apache-2.0)"
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  if (license == "None") {
+    return ""
+  }
+  else {
+    return `\n## License
+    \nThis project is licensed under the ${license}.`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -27,8 +42,7 @@ function generateMarkdown(data) {
   \n${data.install}
   \n## Usage
   \n${data.usage}
-  \n## License
-  \nThis project is licensed under the ${data.license}
+  ${renderLicenseSection(data.license)}
   \n## Contributing
   \n${data.contributing}
   \n## Tests
